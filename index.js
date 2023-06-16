6c12abd30f9cb21cb7df7938e8ef35d86d1a8353
Sourav <souravsemail2001@gmail.com>
2023-06-16 11:58:07 +0530

    made a rapper info api

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+const rappers = {
+  "21 savage": {
+    birthName: "Shéyaa Bin Abraham-Joseph",
+    birthLocation: "London, England",
+    age: 29,
+  },
+  "chance the rapper": {
+    birthName: "Chancelor Johnathan Bennett",
+    birthLocation: "Chicago, Illinois, U.S",
+    age: 29,
+  },
+  eminem: {
+    birthName: "Marshall Bruce Mathers III",
+    birthLocation: "St. Joseph, Missouri",
+    age: 50,
+  },
+};
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/:name", (req, res) => {
+  const rapperName = req.params.name.toLowerCase();
+  if (rappers[rapperName]) {
+    res.json(rappers[rapperName]);
+  } else {
+    res.send("No rapper found with that name");
+  }
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`The server is listening at ${PORT}`);
+});
