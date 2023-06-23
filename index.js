30df3cc47de51a29799de8b1beac749cd1215f15
Sourav <souravsemail2001@gmail.com>
2023-06-24 00:08:51 +0530

    added mongodb as a database and done with creating and reading of crud functionality

--- index.js
+++ index.js
@@ -2,40 +2,45 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = 5000;
+const MongoClient = require("mongodb").MongoClient;
 
-app.use(cors());
+MongoClient.connect(
+  "mongodb+srv://souravsemail2001:mymongo@cluster0.ojwz25g.mongodb.net/?retryWrites=true&w=majority"
+)
+  .then((client) => {
+    app.set("view-engine", "ejs");
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded());
+    console.log("Connected to the database!");
+    const db = client.db("rappers-names-app");
+    const rappers = db.collection("rappers");
+    app.get("/", (req, res) => {
+      rappers
+        .find()
+        .toArray()
+        .then((results) => {
+          res.render("index.ejs", { info: results });
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    });
+    app.post("/form", (req, res) => {
+      rappers
+        .insertOne(req.body)
+        .then((result) => {
+          console.log(result);
+        })
+        .then(() => {
+          res.redirect("/");
+        });
+    });
 
-const rappers = {
-  "21 savage": {
-    birthName: "Shéyaa Bin Abraham-Joseph",
-    birthLocation: "London, England",
-    age: 29,
-  },
-  "chance the rapper": {
-    birthName: "Chancelor Johnathan Bennett",
-    birthLocation: "Chicago, Illinois, U.S",
-    age: 29,
-  },
-  eminem: {
-    birthName: "Marshall Bruce Mathers III",
-    birthLocation: "St. Joseph, Missouri",
-    age: 50,
-  },
-};
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-app.get("/api/:name", (req, res) => {
-  const rapperName = req.params.name.toLowerCase();
-  if (rappers[rapperName]) {
-    res.json(rappers[rapperName]);
-  } else {
-    res.json({ age: 0, birthName: "Unknown", birthLocation: "Unknown" });
-  }
-});
-
-app.listen(process.env.PORT || PORT, () => {
-  console.log(`The server is listening at ${PORT}`);
-});
+    app.listen(process.env.PORT || PORT, () => {
+      console.log(`The server is listening at ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
